640ab7ae4dc22703202ceb00619bc3414527bdab
Rishop Babu <rishop.babu@optisolbusiness.com>
2022-05-31 12:12:19 +0530

    Initial Commit

---
+++ FirebaseTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseTutorial
+//
+//  Created by MAC-OBS-26 on 31/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
