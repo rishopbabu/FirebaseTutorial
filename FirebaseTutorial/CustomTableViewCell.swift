9f26001fce0debf3cb93bb5235031e2864a6b0cc
Rishop Babu <rishop.babu@optisolbusiness.com>
2022-06-01 08:45:42 +0530

    first commit

---
+++ FirebaseTutorial/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  FirebaseTutorial
+//
+//  Created by MAC-OBS-26 on 31/05/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FirebaseTutorial/NameModel.swift
@@ -0,0 +1,8 @@
+//
+//  NameModel.swift
+//  FirebaseTutorial
+//
+//  Created by MAC-OBS-26 on 31/05/22.
+//
+
+import Foundation
